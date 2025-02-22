ed24a115786ae45100a9f3862a8ff1deb9cba05f
Geoo Chi <geoooochi@gmail.com>
2025-02-22 12:42:49 +0800

    core

---
+++ src/index.tsx
@@ -0,0 +1,44 @@
+import { createRoot } from 'react-dom/client'
+import QRCode from 'qrcode'
+import { useState } from 'react'
+import React from 'react'
+import * as OTPAuth from 'otpauth'
+
+const App = () => {
+  const [url, setUrl] = useState('')
+  const [text, setText] = useState('')
+  const [intervalNumber, setIntervalNumber] = useState<NodeJS.Timeout | null>(
+    null
+  )
+  const generateHandler = async () => {
+    const secret = new OTPAuth.Secret({ size: 20 })
+    const totp = new OTPAuth.TOTP({
+      issuer: 'PeerAuth',
+      label: encodeURIComponent('test'),
+      algorithm: 'SHA1',
+      digits: 6,
+      period: 30,
+      secret,
+    })
+    setUrl(await QRCode.toDataURL(totp.toString()))
+    function updateOtpDisplay() {
+      const now = Math.floor(Date.now() / 1000)
+      const remaining = totp.period - (now % totp.period)
+
+      setText(`${totp.generate()} (expires in ${remaining} seconds)`)
+    }
+    updateOtpDisplay()
+    if (intervalNumber) clearInterval(intervalNumber)
+    setIntervalNumber(setInterval(updateOtpDisplay, 1000))
+  }
+
+  return (
+    <>
+      <button onClick={generateHandler}>Generate</button>
+      <img src={url} />
+      <p>{text}</p>
+    </>
+  )
+}
+
+createRoot(document.getElementById('root')!).render(<App />)
